419fb9701dfd3e936a50877baed17f419547d9df
Tey Bannerman <tey.bannerman@gmail.com>
2014-06-01 16:28:40 +0200

    Set up imgur API connectivity & image loading
    - Connect to imgur to obtain meme subgallery images
    - Add preloader JS for imgur images

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,9 @@
 
   $(function() {
 
-    var SOUNDCLOUD_ID, USER_ID;
+    var SOUNDCLOUD_ID, USER_ID, IMGUR_KEY;
     SOUNDCLOUD_ID = "ddd1a518196f22519fc5ca02ca4a4fc7";
+    IMGUR_KEY = "d93eaa3187ee692";
 
     SC.initialize({
       client_id: SOUNDCLOUD_ID,
@@ -20,6 +21,7 @@
             $('.panel-selector h1').append(user.username);
             $('.panel').hide(500);
             getTracks(USER_ID);
+            getImages(IMGUR_KEY);
           }
         });
       });
@@ -38,5 +40,20 @@ function getTracks(uid) {
       $('.app-nav').show(500);
       console.log(tracks);
     }
+  });
+}
+
+function getImages(key, cat) {
+  $.ajax({
+    url: 'https://api.imgur.com/3/gallery/g/memes',
+    type: 'GET',
+    headers: {
+      Authorization: 'Client-ID ' + key,
+    },
+    dataType: 'json'
+  }).success(function(data) {
+    console.log(data);
+  }).error(function() {
+    alert('Could not reach api.imgur.com. Sorry :(');
   });
 }
